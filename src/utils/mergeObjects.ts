8475451796adf21bc8edc4560fdba14de95ec79a
johnsmith0212 <john0212.skill@gmail.com>
2023-12-27 13:10:57 +0000

    init

---
+++ src/utils/mergeObjects.ts
@@ -0,0 +1,15 @@
+// Wrapper on `Object.assign` to throw error if keys clash
+const mergeObjects = <T extends object, U extends object>(
+  target: T,
+  newObject: U
+): T & U => {
+  const targetKeys = Object.keys(target)
+  for (const key of Object.keys(newObject)) {
+    if (targetKeys.includes(key)) {
+      throw new Error(`target object already has key: ${key}`)
+    }
+  }
+  return Object.assign(target, newObject)
+}
+
+export default mergeObjects
